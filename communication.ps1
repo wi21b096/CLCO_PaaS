8bc1516f8cccd6e6916e6ec6c539daf8c8b8baf0
Raphael Koller <wi21b096@technikum-wien.at>
2023-09-27 18:18:58 +0200

    Initial

---
+++ communication.ps1
@@ -0,0 +1,26 @@
+az login
+az cognitiveservices account show --resource-group clco_grp6 --name clco-grp6-webapp-service --query id --output tsv
+az network vnet create --resource-group clco_grp6 --location LOCATION --name clco_grp6_network --address-prefixes 10.0.0.0/16
+az network vnet subnet create --resource-group clco_grp6 --vnet-name clco_grp6_network --name clco_grp6_network_v1 --address-prefixes 10.0.0.0/24 --delegations Microsoft.Web/serverfarms --disable-private-endpoint-network-policies false
+az network vnet subnet create --resource-group clco_grp6 --vnet-name clco_grp6_network --name clco_grp6_network_v2 --address-prefixes 10.0.1.0/24 --disable-private-endpoint-network-policies true
+az network private-dns zone create --resource-group clco_grp6 --name "privatelink.cognitiveservices.azure.com"
+az network private-dns link vnet create --resource-group clco_grp6 --name cognitiveservices-zonelink --zone-name privatelink.cognitiveservices.azure.com --virtual-network clco_grp6_network --registration-enabled False
+az resource update --ids clco_grp6_network_v2 --set properties.publicNetworkAccess="Disabled"
+az webapp update --resource-group clco_grp6 --name clco-grp6-webapp --https-only
+az webapp vnet-integration add --resource-group clco_grp6 --name clco-grp6-webapp --vnet clco_grp6_network_v1 --subnet clco_grp6_network
+# Get the endpoint for the Language service resource
+> endpoint=$(az cognitiveservices account show \
+--name clco-grp6-webapp-service \
+--resource-group clco_grp6 \
+--query "properties.endpoint" \
+--output tsv
+)
+# Get the associated key
+> key=$(az cognitiveservices account keys list \
+--name clco-grp6-webapp-service \
+--resource-group clco_grp6 \
+--query "key1" \
+--output tsv
+)
+> az webapp config appsettings set --resource-group clco_grp6 --name clco-grp6-webapp --settings AZ_ENDPOINT="$endpoint" AZ_KEY="$key"
+# az group delete --name clco_grp6 # delete azure group
---
+++ compute.ps1
@@ -0,0 +1,7 @@
+# winget install git.git # install git
+git clone https://github.com/dmelichar/clco-demo # one time clone of webapp
+cd .\clco-demo\ # change directory for webapp up upload
+az webapp up --runtime PYTHON:3.9 --sku B1 --location westeurope --resource-group clco_grp6 --name clco-grp6-webapp
+az webapp log config --web-server-logging filesystem --name clco-grp6-webapp --resource-group clco_grp6 # configure logging
+az webapp log tail --name clco-grp6-webapp --resource-group clco_grp6 # real-time monitoring of logs
+# az group delete --name clco_grp6 # delete azure group
