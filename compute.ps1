000bf50320cae8f6b5149d150dc4e38e69261135
Raphael Koller <wi21b096@technikum-wien.at>
2023-09-27 18:28:10 +0200

    Fix

--- compute.ps1
+++ compute.ps1
@@ -1,4 +1,5 @@
 # winget install git.git # install git
+az login
 git clone https://github.com/dmelichar/clco-demo # one time clone of webapp
 cd .\clco-demo\ # change directory for webapp up upload
 az webapp up --runtime PYTHON:3.9 --sku B1 --location westeurope --resource-group clco_grp6 --name clco-grp6-webapp
