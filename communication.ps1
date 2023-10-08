0c530fa64b95543db4e917f0b679cae779700d71
Raphael Koller <koller.raphael@pm.me>
2023-10-08 18:42:31 +0200

    Update

--- communication.ps1
+++ communication.ps1
@@ -1,4 +1,5 @@
 # az login --use-device-code
+az cognitiveservices account create --name clco-grp6-ai-service --kind TextAnalytics --sku B1 --location westeurope --resource-group clco_grp6 --yes
 az cognitiveservices account show --resource-group clco_grp6 --name clco-grp6-ai-service --query id --output tsv
 az network vnet create --resource-group clco_grp6 --location westeurope --name clco_grp6_network --address-prefixes 10.0.0.0/16
 az network vnet subnet create --resource-group clco_grp6 --vnet-name clco_grp6_network --name clco_grp6_network_v1 --address-prefixes 10.0.0.0/24 --delegations Microsoft.Web/serverfarms --disable-private-endpoint-network-policies false
@@ -6,9 +7,15 @@ az network vnet subnet create --resource-group clco_grp6 --vnet-name clco_grp6_n
 az network private-dns zone create --resource-group clco_grp6 --name "privatelink.cognitiveservices.azure.com"
 az network private-dns link vnet create --resource-group clco_grp6 --name cognitiveservices-zonelink --zone-name privatelink.cognitiveservices.azure.com --virtual-network clco_grp6_network --registration-enabled False
 az resource patch --ids "/subscriptions/7d3b23f7-171d-40d3-b2a0-f0a9d98ddee0/resourceGroups/clco_grp6/providers/Microsoft.CognitiveServices/accounts/clco-grp6-ai-service" --properties "{ \"publicNetworkAccess\": \"Disabled\"}"
-az webapp update --resource-group clco_grp6 --name clco-grp6-webapp --https-only
-az webapp vnet-integration add --resource-group clco_grp6 --name clco-grp6-webapp --vnet clco_grp6_network --subnet clco_grp6_network_v1
+az webapp update --resource-group clco_grp6 --name clco-grp6-webapp-1 --https-only
+az webapp update --resource-group clco_grp6 --name clco-grp6-webapp-2 --https-only
+az webapp update --resource-group clco_grp6 --name clco-grp6-webapp-3 --https-only
+az webapp vnet-integration add --resource-group clco_grp6 --name clco-grp6-webapp-1 --vnet clco_grp6_network --subnet clco_grp6_network_v1
+az webapp vnet-integration add --resource-group clco_grp6 --name clco-grp6-webapp-2 --vnet clco_grp6_network --subnet clco_grp6_network_v1
+az webapp vnet-integration add --resource-group clco_grp6 --name clco-grp6-webapp-3 --vnet clco_grp6_network --subnet clco_grp6_network_v1
 az cognitiveservices account show --name clco-grp6-ai-service --resource-group clco_grp6 --query "properties.endpoint" --output tsv
 az cognitiveservices account keys list --name clco-grp6-ai-service --resource-group clco_grp6 --query "key1" --output tsv
-az webapp config appsettings set --resource-group clco_grp6 --name clco-grp6-webapp --settings AZ_ENDPOINT="https://clco-grp6-ai-service.cognitiveservices.azure.com/" AZ_KEY="ccd6315aed6842b0884b9775c5077f55"
+az webapp config appsettings set --resource-group clco_grp6 --name clco-grp6-webapp-1 --settings AZ_ENDPOINT="https://clco-grp6-ai-service.cognitiveservices.azure.com/" AZ_KEY="ccd6315aed6842b0884b9775c5077f55"
+az webapp config appsettings set --resource-group clco_grp6 --name clco-grp6-webapp-2 --settings AZ_ENDPOINT="https://clco-grp6-ai-service.cognitiveservices.azure.com/" AZ_KEY="ccd6315aed6842b0884b9775c5077f55"
+az webapp config appsettings set --resource-group clco_grp6 --name clco-grp6-webapp-3 --settings AZ_ENDPOINT="https://clco-grp6-ai-service.cognitiveservices.azure.com/" AZ_KEY="ccd6315aed6842b0884b9775c5077f55"
 # az group delete --name clco_grp6 # delete azure group
--- compute.ps1
+++ compute.ps1
@@ -2,7 +2,9 @@
 # az login --use-device-code
 # git clone https://github.com/dmelichar/clco-demo # one time clone of webapp
 cd .\clco-demo\ # change directory for webapp up upload
-az webapp up --runtime PYTHON:3.9 --sku B1 --location westeurope --resource-group clco_grp6 --name clco-grp6-webapp
-az webapp log config --web-server-logging filesystem --name clco-grp6-webapp --resource-group clco_grp6 # configure logging
-az webapp log tail --name clco-grp6-webapp --resource-group clco_grp6 # real-time monitoring of logs
+az webapp up --runtime PYTHON:3.9 --sku B1 --location westeurope --resource-group clco_grp6 --name clco-grp6-webapp-1 --plan clco-grp6-appsvcplan
+az webapp up --runtime PYTHON:3.9 --sku B1 --location westeurope --resource-group clco_grp6 --name clco-grp6-webapp-2 --plan clco-grp6-appsvcplan
+az webapp up --runtime PYTHON:3.9 --sku B1 --location westeurope --resource-group clco_grp6 --name clco-grp6-webapp-3 --plan clco-grp6-appsvcplan
+# az webapp log config --web-server-logging filesystem --name clco-grp6-webapp --resource-group clco_grp6 # configure logging
+# az webapp log tail --name clco-grp6-webapp --resource-group clco_grp6 # real-time monitoring of logs
 # az group delete --name clco_grp6 # delete azure group
