94188c559e654acdec582acb01ca6a7fd5f9ec22
Raphael Koller <koller.raphael@pm.me>
2023-10-08 17:38:40 +0200

    Fix

--- communication.ps1
+++ communication.ps1
@@ -5,12 +5,10 @@ az network vnet subnet create --resource-group clco_grp6 --vnet-name clco_grp6_n
 az network vnet subnet create --resource-group clco_grp6 --vnet-name clco_grp6_network --name clco_grp6_network_v2 --address-prefixes 10.0.1.0/24 --disable-private-endpoint-network-policies true
 az network private-dns zone create --resource-group clco_grp6 --name "privatelink.cognitiveservices.azure.com"
 az network private-dns link vnet create --resource-group clco_grp6 --name cognitiveservices-zonelink --zone-name privatelink.cognitiveservices.azure.com --virtual-network clco_grp6_network --registration-enabled False
-# az resource update --ids "/subscriptions/7d3b23f7-171d-40d3-b2a0-f0a9d98ddee0/resourceGroups/clco_grp6/providers/Microsoft.CognitiveServices/accounts/clco-grp6-ai-service" --set properties.publicNetworkAccess="Disabled" # not working with disabled flag
+az resource patch --ids "/subscriptions/7d3b23f7-171d-40d3-b2a0-f0a9d98ddee0/resourceGroups/clco_grp6/providers/Microsoft.CognitiveServices/accounts/clco-grp6-ai-service" --properties "{ \"publicNetworkAccess\": \"Disabled\"}"
 az webapp update --resource-group clco_grp6 --name clco-grp6-webapp --https-only
 az webapp vnet-integration add --resource-group clco_grp6 --name clco-grp6-webapp --vnet clco_grp6_network --subnet clco_grp6_network_v1
-# Get the endpoint for the Language service resource
 az cognitiveservices account show --name clco-grp6-ai-service --resource-group clco_grp6 --query "properties.endpoint" --output tsv
-# Get the associated key
 az cognitiveservices account keys list --name clco-grp6-ai-service --resource-group clco_grp6 --query "key1" --output tsv
 az webapp config appsettings set --resource-group clco_grp6 --name clco-grp6-webapp --settings AZ_ENDPOINT="https://clco-grp6-ai-service.cognitiveservices.azure.com/" AZ_KEY="ccd6315aed6842b0884b9775c5077f55"
 # az group delete --name clco_grp6 # delete azure group
